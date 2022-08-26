66590ad59d2f2f31ca87956d7a1e0dff71f705f7
abutair <mohammedabutair989@gmail.com>
2022-08-26 09:41:40 +0300

    '#' will be ignored, and an empty message aborts the commit.

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -85,10 +85,6 @@ class PrivateRecipeApiTests(TestCase):
 
     def test_recipe_list_limited_to_user(self):
         """Test list of recipes is limited to authenticated user."""
-        other_user = get_user_model().objects.create_user(
-            'other@example.com',
-            'password123',
-        )
         other_user = create_user(email='other@example.com', password='test123')
         create_recipe(user=other_user)
         create_recipe(user=self.user)
