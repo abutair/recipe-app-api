988419864150eff4f1dd09f94f0e3b110b8e735e
abutair <mohammedabutair989@gmail.com>
2022-09-04 23:35:31 +0300

    fixing duplicate table issue

--- app/core/models.py
+++ app/core/models.py
@@ -3,12 +3,14 @@ Database models.
 """
 import uuid
 import os
+
 from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
-    PermissionsMixin)
+    PermissionsMixin,
+)
 
 
 def recipe_image_file_path(instance, filename):
@@ -20,7 +22,8 @@ def recipe_image_file_path(instance, filename):
 
 
 class UserManager(BaseUserManager):
-    """Manager for user."""
+    """Manager for users."""
+
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
         if not email:
@@ -28,14 +31,16 @@ class UserManager(BaseUserManager):
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
+
         return user
 
     def create_superuser(self, email, password):
-        """Create and return a new superuser"""
+        """Create and return a new superuser."""
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
+
         return user
 
 
@@ -45,7 +50,9 @@ class User(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+
     objects = UserManager()
+
     USERNAME_FIELD = 'email'
 
 
@@ -85,8 +92,8 @@ class Ingredient(models.Model):
     name = models.CharField(max_length=255)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
     )
 
     def __str__(self):
-        return  self.name
+        return self.name
